63e5dc013b0ec331fbada4b1a1c4271f05279306
mgrkapi <mgrkapi@wp.pl>
2022-06-09 17:39:36 +0200

    bug fix

--- roomsofpoland/src/components/Header.js
+++ roomsofpoland/src/components/Header.js
@@ -3,16 +3,17 @@ import "../style/header.scss";
 import logo from "../images/logo.png"
 import {Link} from "react-router-dom";
 import about from "../about";
+import rooms from "../rooms";
 
 export function Header() {
     return (
-        <header>
+        <header class="header__main">
             <div className="container">
                 <div className="logo"><img src={logo} alt= "logo"/></div>
                 <div className = "menu">
                     <ul>
                         <li ><Link to ="/about">O nas</Link></li>
-                        <li ><Link to ="/">Pokoje</Link></li>
+                        <li ><Link to ="/rooms">Pokoje</Link></li>
                         <li ><Link to ="/">Cennik/Rezerwacje</Link></li>
                         <li ><Link to ="/">Atrakcje</Link></li>
                         <li ><Link to ="/">Opinie</Link></li>
--- roomsofpoland/src/components/Home.js
+++ roomsofpoland/src/components/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 import Header from "./Header";
 
 function Home () {
-    return <Header/>
+    return (
+        <Header/>
+    )
 }
 
 export default Home;
