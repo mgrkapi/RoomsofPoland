189d3c68aad51c7c64ae21b581ace408179734eb
mgrkapi <mgrkapi@wp.pl>
2022-06-16 13:56:53 +0200

    Modified styles in content and blurb,added links to Blurb and text to Main

--- roomsofpoland/src/components/Main.js
+++ roomsofpoland/src/components/Main.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "../style/content.scss";
-import JPG from "../images/main.jpg";
-function Main () {
+import main from "../images/main.jpg";
+
+function Main() {
     return (
         <>
-        <section className = "main__content">
-            <div className="main"><img src={JPG} alt="room"/></div>
-            <h1>Witamy w Rooms of Poland</h1>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-        </section>
+            <section className="main__content">
+                <div className="main"><img src={main} alt="room"/></div>
+                <h1>Witamy w Rooms of Poland!</h1>
+                <p>Rooms of Poland to idealne miejsce na odpoczynek dla osób lubiących kontakt z naturą, a jednocześnie
+                    ceniących bliską odległość do różnego rodzaju atrakcji.</p>
+                <p>Tutaj zapomną Państwo o miejskim hałasie, zatopią się w ciszy i znajdą wyjątkowe miejsca na długie
+                    spacery, wycieczki oraz wiele okolicznych atrakcji!</p>
+                <p>Każdy może poczuć się niepowtarzalnie poprzez indywidualny charakter wnętrz oraz wszelkie starania,
+                    jakich dokładamy, by zapewnić Państwu mile spędzony czas.</p>
+                <p>Pokoje zaprojektowane zostały z sercem, a każdy z nich opowiada swoją własną historię, którą
+                    najlepiej odkryć wieczorem przy ognisku.</p>
+            </section>
         </>
     )
 }
