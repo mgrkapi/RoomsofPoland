490f5bb496f97bde46c9d8a7badd34f069d61f59
mgrkapi <mgrkapi@wp.pl>
2022-07-18 14:42:02 +0200

    Made changes in the footer content and styles

--- roomsofpoland/src/components/Footer.js
+++ roomsofpoland/src/components/Footer.js
@@ -3,33 +3,27 @@ import logo from "../images/logo.png"
 import "../style/footer.scss"
 import facebook from "../images/facebook.png"
 
-function Footer () {
+function Footer() {
     return (
         <footer>
-            <div className="container">
+            <div>
                 <div className="footer">
                     <div className="logo">
-                        <img src={logo} alt= "logo"/>
+                        <img src={logo} alt="logo"/>
                     </div>
                     <div className="address">
-                        <p>
-                            Rooms of Poland
-                            <br/>
-                            96 - 300 Krzyżówka
-                            <br/>
-                            ul. Wypoczynkowa 4
-                            <br/>
-                            + 48 507 004 901
-                            <br/>
-                            email: roomsofpoland@gmail.com
-                        </p>
+                        <h3>Adres</h3>
+                        <p>Rooms of Poland</p>
+                        <p>ul. Wypoczynkowa 4</p>
+                        <p>96 - 300 Krzyżówka</p>
                     </div>
-                    <div className="information">
-                        <h3>Informacje</h3>
-                        <a href = "#">Regulamin obiektu</a>
+                    <div className="contact">
+                        <h3>Kontakt</h3>
+                        <p>+ 48 507 004 901</p>
+                        <p>email: roomsofpoland@gmail.com</p>
                     </div>
                     <div className="links">
-                        <a href = "https://www.facebook.com/roomsofpoland"><img src ={facebook} alt = "Facebook"/></a>
+                        <a href="https://www.facebook.com/roomsofpoland"><img src={facebook} alt="Facebook"/></a>
                     </div>
                 </div>
             </div>
